5c785edece9e0db654f1398cdd8fd4b71dd2bd76
Qingnan Zhou <qzhou@adobe.com>
2026-06-03 16:57:55 -0400

    Update dependency.

--- cmake/conformal_ideal_delaunay.cmake
+++ cmake/conformal_ideal_delaunay.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
     conformal_ideal_delaunay
     SYSTEM
-    GIT_REPOSITORY https://github.com/rjc8237/ConformalIdealDelaunay.git
+    GIT_REPOSITORY https://github.com/qnzhou/ConformalIdealDelaunay.git
     GIT_TAG penner_optimization
 )
 FetchContent_MakeAvailable(conformal_ideal_delaunay)
